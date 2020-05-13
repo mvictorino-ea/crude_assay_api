e387f38bfb00608bf78a1d1faf19590ee66158fd
Marcello Victorino <marcello.victorino@energyaspects.com>
2020-05-13 14:05:32 +0100

    add: Report Date as query param. Note: SJ operator (@repdate) not working

--- apis/crude_supply.py
+++ apis/crude_supply.py
@@ -17,8 +17,7 @@ parser_crude_supply_get.add_argument('date_from', type=str, required=False, help
 parser_crude_supply_get.add_argument('date_to', type=str, required=False, help='2030-01-01 OR +3M')
 
 #TODO: how to retrieve latest, second latest etc (i.e: -1)
-# there is a specific function for that -> get_reported_date
-# Look the documentation
+# there is a specific function for that -> conn.get_reported_date()
 parser_crude_supply_get.add_argument('report_date', type=str, required=False, help='2018-01-12 OR -1db +2h')
 
 
@@ -35,6 +34,7 @@ class Base:
                            api_key=os.getenv('SHOOJU_KEY'))
 
     # Define standard fields
+    # TODO: double-check desired meta fields to be returned
     fields = ['country', 'country_iso', 'region', 'energy_product', 'unit', 'description']
 
     # Base SID for Crude Oil Supply - OPEC
@@ -60,6 +60,7 @@ class Crude(Resource, Base):
         countries = args['countries']
         date_from = args.get('date_from')
         date_to = args['date_to']
+        report_date = args['report_date']
 
         # Set Identifier Precedence
         if countries:
@@ -77,8 +78,13 @@ class Crude(Resource, Base):
         else:
             param_date_to = ''
 
+        if report_date:
+            param_report_date = f'@repdate:{report_date}'
+        else:
+            param_report_date = ''
+
         # Prepare SID
-        sid = self.base_sid + param_identifier + param_date_from + param_date_to
+        sid = self.base_sid + param_identifier + param_date_from + param_date_to + param_report_date
 
         # Retrieve multiple results/series
         fields, points = [], []
@@ -103,3 +109,49 @@ class Crude(Resource, Base):
         self.response = df.to_json(orient='columns', date_format='iso', indent=2)
 
         return self.response, 200
+
+
+#TODO: Developing
+
+# api = Base()
+#
+# param = (r' country=(Algeria,Congo,Venezuela,Kuwait)'
+#          r'@df:2018-01-01 @dt:2019-12-01')
+#
+# sid = api.base_sid + param
+#
+# #
+# # Retrieve multiple results/series
+# fields, points, repdates = [], [], []
+# for s in api.sj.scroll(sid, fields=api.fields, max_points=-1, serializer=shooju.points_serializers.pd_series):
+#     # Extract available reported dates for each SID
+#     repdate = api.sj.get_reported_dates(s['series_id'])
+#
+#     # Make sure report date is sorted ascending
+#     repdate.sort()
+#
+#     # Convert to string date_iso format
+#     repdate = [x.strftime('%Y-%m-%d') for x in repdate]
+#
+#     # Store as dictionary
+#     repdate = {'repdate': repdate}
+#
+#     repdates.append(repdate)
+#     fields.append(s.get('fields'))
+#     points.append(s.get('points'))
+#
+# # Group multiple series in a Dataframe
+# # Points
+# df_points = pd.concat(points, axis=1)
+#
+# # Series
+# df_fields = pd.DataFrame(fields).T  # Transpose
+#
+# # List of Reported Dates
+# df_repdate = pd.DataFrame(repdates).T  # Transpose
+#
+# df = pd.concat([df_fields, df_points, df_repdate])
+# # Rename Columns
+# df.columns = [x['country'] for x in fields]
+#
+# pass
